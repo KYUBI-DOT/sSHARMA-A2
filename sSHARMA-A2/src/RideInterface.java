a81e52ae7d4de26f84bb04244971bad6f0f1c793
KYUBI-DOT <133933242+KYUBI-DOT@users.noreply.github.com>
2024-11-28 12:01:55 +0800

    ride interface created

---
+++ sSHARMA-A2/src/RideInterface.java
@@ -0,0 +1,12 @@
+//Operations for ride
+
+public interface RideInterface {
+    void addVisitorToQueue(Visitor visitor);
+    void removeVisitorFromQueue();
+    void printQueue();
+    void runOneCycle();
+    void addVisitorToHistory(Visitor visitor);
+    boolean checkVisitorFromHistory(Visitor visitor);
+    int numberOfVisitors();
+    void printRideHistory();
+}
